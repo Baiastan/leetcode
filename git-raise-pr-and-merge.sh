7054c1265870c9bfcdbba3f76c80e08ceec50ed7
Baiastan Zhuzupbekov <baiastan@Baiastans-MacBook-Pro.local>
2024-10-08 22:06:14 -0700

    updates

--- git-raise-pr-and-merge.sh
+++ git-raise-pr-and-merge.sh
@@ -3,7 +3,7 @@
 current_branch=$(git rev-parse --abbrev-ref HEAD)
 
 echo "Raising a pr to master"
-gh pr create --base master --head "$current_branch" --title "GH updates to master" --body "growth hungry updates"
+gh pr create --base master --head "$current_branch" --title "leetcode updates to master" --body "leetcode updates"
 
 pr_number=$(gh pr list --limit 1 --json number --jq '.[0].number')
 
